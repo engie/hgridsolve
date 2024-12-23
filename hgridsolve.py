6f6d1c337c9f2da60b78d85f593be11c2453417a
Stephen English <steve@secomputing.co.uk>
2024-12-23 00:25:22 +0000

    minor tidy

--- hgridsolve.py
+++ hgridsolve.py
@@ -13,51 +13,59 @@ animals = [
 ]
 vowel_animals = [a for a in animals if a[0] in ['A', 'E', 'I', 'O', 'U']]
 
-grid = {i : animals.copy() for i in range(9)}
-
-def pos(x,y):
-    return x+3*y
-
-def print_grid(g, title = None, depth=0):
-    print('\t' * depth, title)
-    for y in range(3):
-        for x in range(3):
-            if len(g[pos(x, y)]) == 0:
-                s = 'XXX'
-            else:
-                s = ','.join([s[0] for s in sorted(g[pos(x,y)])])
-            s = s + " " * (16-len(s))
-            print('\t' * depth, s, end='\t\t')
-        print()
-    print()
-
-print_grid(grid, 'Start with a full grid')
-print('Setting Goat & Hippo')
-grid[pos(1, 1)] = ['Goat']
-grid[pos(2, 2)] = ['Hippopotamus']
-
-def get_known(g, x, y):
-    values = g[pos(x, y)]
-    if len(values) == 1:
-        return values[0]
-    return None
-
-def remove_val(g, x, y, to_remove):
-    remove_vals(g, x, y, [to_remove])
-
-def remove_vals(g, x, y, to_remove):
-    for r in to_remove:
-        if r in g[pos(x, y)]:
-            g[pos(x, y)].remove(r)
-
 def positions():
+    """
+    All the valid positions in the 3x3 grid
+    """
     positions = []
     for y in range(3):
         for x in range(3):
             positions.append((x, y))
     return positions
 
+def print_grid(g, title = None, depth=0):
+    """
+    Print out the grid, a bit nicely.
+    """
+    print('\t' * depth, title)
+    for y in range(3):
+        for x in range(3):
+            if len(g[(x, y)]) == 0:
+                s = 'XXX'
+            else:
+                s = ','.join([s[0] for s in sorted(g[(x,y)])])
+            s = s + " " * (16-len(s))
+            print('\t' * depth, s, end='\t\t')
+        print()
+    print()
+
+def get_known(g, x, y):
+    """
+    Value of a cell if it's known (only one possibility), otherwise None
+    """
+    values = g[(x, y)]
+    if len(values) == 1:
+        return values[0]
+    return None
+
+def remove_val(g, x, y, to_remove):
+    """
+    Remove a possibility from a cell.
+    """
+    remove_vals(g, x, y, [to_remove])
+
+def remove_vals(g, x, y, to_remove):
+    """
+    Remove a list of possibilities from a cell
+    """
+    for r in to_remove:
+        if r in g[(x, y)]:
+            g[(x, y)].remove(r)
+
 def known_positions(g):
+    """
+    All the positions where we know the value (and that value)
+    """
     known = []
     for x, y in positions():
         animal = get_known(g, x, y)
@@ -152,16 +160,9 @@ def rule_cheetah_goat_same_row(g):
                 if j != y:
                     remove_vals(g, i, j, looking_for)
 
-print()
-rule_alligator_in_bottom_row(grid)
-rule_duck_not_on_bottom(grid)
-rule_fox_not_in_third_col(grid)
-rule_iguana_is_in_corner(grid)
-print_grid(grid, 'Cleared obvious rules')
-
 def has_empty_cells(g):
     for x, y in positions():
-        if len(g[pos(x, y)]) == 0:
+        if len(g[(x, y)]) == 0:
             return True
     return False
 
@@ -169,20 +170,20 @@ def finished(g):
     return len(known_positions(g)) == 9
 
 def apply_rules(g):
+    count_of_known_positions_before_rules = len(known_positions(g))
+    rule_there_can_only_be_one(g)
+    rule_no_alphabetical_neighbour(g)
+    rule_no_two_vowels(g)
+    rule_beaver_eagle_same_col(g)
+    rule_cheetah_goat_same_row(g)
     # Keep applying rules until known cells stable
-    last_known_position_count = None
-    while last_known_position_count != len(known_positions(g)):
-        last_known_position_count = len(known_positions(g))
-        rule_there_can_only_be_one(g)
-        rule_no_alphabetical_neighbour(g)
-        rule_no_two_vowels(g)
-        rule_beaver_eagle_same_col(g)
-        rule_cheetah_goat_same_row(g)
+    if count_of_known_positions_before_rules != len(known_positions(g)):
+        apply_rules(g)
 
 def step(g, depth):
-    print_grid(g, 'Depth: ' + str(depth), depth)
+    #print_grid(g, 'Depth: ' + str(depth), depth)
     apply_rules(g)
-    print_grid(g, 'After rules', depth)
+    #print_grid(g, 'After rules', depth)
     if has_empty_cells(g):
         return None
     if finished(g):
@@ -192,20 +193,31 @@ def step(g, depth):
     guesses = []
     for y in range(3):
         for x in range(3):
-            values = g[pos(x, y)]
+            values = g[(x, y)]
             if len(values) > 1:
                 for v in values:
                     guesses.append((x, y, v))
     for x, y, v in guesses:
         new_g = copy.deepcopy(g)
-        print('Tweaking ', x, y, v)
-        if x == 0 and y == 1 and v == 'Cheetah' and depth==3:
-            print('omg')
-        new_g[pos(x, y)] = [v]
+        new_g[(x, y)] = [v]
         result = step(new_g, depth+1)
         if result != None:
             return result
     return None
+
+# Grid starts with all the animals as possible for all the cells
+grid = {(x, y) : animals.copy() for x, y in positions()}
+print_grid(grid, 'Start with a full grid')
+print('Setting Goat & Hippo')
+grid[(1, 1)] = ['Goat']
+grid[(2, 2)] = ['Hippopotamus']
+
+print()
+rule_alligator_in_bottom_row(grid)
+rule_duck_not_on_bottom(grid)
+rule_fox_not_in_third_col(grid)
+rule_iguana_is_in_corner(grid)
+print_grid(grid, 'Cleared obvious rules')
 
 result = step(grid, 0)
 assert(result != None)
